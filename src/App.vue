dd6fa732adc2e5b396794113c7209fb7111230a9
Mykola Tokarskyi <mykola.tokarskyi@trisbee.com>
2023-04-06 09:40:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="content">
+    <div>Likes count {{ likes }}</div>
+    <button @click="addLikes">like</button>
+    <button @click="addDisLikes">dislike</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0
+    }
+  },
+  methods: {
+    addLikes(){
+      this.likes += 1
+    },
+    addDisLikes(){
+      this.likes -= 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+.content{
+  display: flex;
+  flex-direction: column;
+
+}
+</style>
