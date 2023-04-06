fc5066ec5d0a2f3bad08040bb6038d4cf2b49d89
Mykola Tokarskyi <mykola.tokarskyi@trisbee.com>
2023-04-06 11:20:03 +0200

    create posts

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-    name: 'posts'
+    name: 'post-form'
 }
 </script>
 <style lang="">
---
+++ src/components/PostList.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div class="posts" v-for="post in posts">
+        <div class="post">
+          <p>{{ post.title }}</p>
+          <p>{{ post.description }}</p>
+        </div>
+      </div>
+</template>
+<script>
+export default {
+    name: 'posts-list'
+}
+</script>
+<style lang="">
+    
+</style>
